a84f3def16dd59cf8c71fa4b8508ffbf8553b6d5
RayenJabri <120359433+RayenJabri@users.noreply.github.com>
2023-10-15 00:06:51 +0100

    Add files via upload

---
+++ src/app/produits/produits.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Produit } from '../model/produit.model';
+import { ProduitService } from '../services/produit.service';
+
+@Component({
+  selector: 'app-produits',
+  templateUrl: './produits.component.html',
+  styleUrls: ['./produits.component.css']
+})
+export class ProduitsComponent implements OnInit {
+  produits? : Produit[];
+constructor(private produitService: ProduitService ) {
+
+}
+
+  ngOnInit(): void {
+    this.produits = this.produitService.listeProduits();
+  }
+  supprimerProduit(p: Produit)
+  {
+  //console.log(p);
+  let conf = confirm("Etes-vous sûr ?");
+ if (conf)
+ this.produitService.supprimerProduit(p);
+  }
+}
